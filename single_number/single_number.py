45e907fa9b7e3200d3beaedebb6e1196dab068a0
Tyler Alsop <tylerjalsop@gmail.com>
2020-07-14 18:48:40 -0600

    Made attempt at single_number(). Not yet working.

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,9 +4,27 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
-    
+    # U:
+        # Recieves a list of numbers where each number except for one is in the list twice.
+        # Function needs to return the integer that appears in the list only once.
+    # P:
+        # Need to loop through the list
+        # For each number we need to find out of that same number exists anywhere else in the list.
+            # If the number does not exist anywhere else in the list then we return that number.
+    # E:
+    i = 0
+    j = 0
+    while len(arr) > 1:
+        current_index = arr[i]
 
-    pass
+        for j in arr:
+            arr.remove(current_index)
+            if j == current_index:
+                arr.remove(j)
+                i += 1
+            else:
+                return j
+
 
 
 if __name__ == '__main__':
