242fba7174eb9c3003ed260bb5f23f01d8dc6ae3
Tyler Alsop <tylerjalsop@gmail.com>
2020-07-16 17:38:53 -0600

    Attempted moving_zeroes(). Code not working / tests not passing. Going to try again.

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,26 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    # U:
+        # Recieves a list of numbers.
+        # Function needs to move all non-zero numbers to the right of the array.
+    # P:
+        # For each of the non-zero numbers they need to be moved to the 
+    # E:
+    temp_index = 0
+    for i in arr:
+        print(arr)
+        if arr[i] != 0:
+            arr[temp_index] = arr[i]
+            temp_index += 1
+            print(arr)
 
-    pass
+    while temp_index < len(arr):
+        arr[temp_index] = 0
+        temp_index += 1
+            
+    print(arr)
+
 
 
 if __name__ == '__main__':
