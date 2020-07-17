8b68c37f64f1bc5c3ce31a64a5c4e85983f0f754
Tyler Alsop <tylerjalsop@gmail.com>
2020-07-17 10:14:05 -0600

    Finished moving_zeroes(). Tests passing.

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -11,16 +11,31 @@ def moving_zeroes(arr):
         # For each of the non-zero numbers they need to be moved to the right of the array.
         ##########################################################################################
         # The above plan failed. New Plan:
-            # Create a filter
+            # Create a variable for the current index.
+            # Create a variable for the array length which will be used for the last index in the array.
+            # Create a while loop for as long as the array index is less than the array length
+                # If the current index is zero
+                    # We need to remove the zero and append it to the end of the array. Will use .pop() and .append()
+                    # Then decrease the array length because we no longer want to access the zero appended to the end.
+                # If the current index is a non-zero number (else statement):
+                    # We need to increase the current index for the while loop.
+                # Return the adjusted array.
+
     # E:
-    temp_arr_1 = []
-    count = 0
-    for i in arr:
-        if arr[i] != 0:
-            arr[count + 1] = arr[i]
-    while count < arr_length:
-        arr[count + 1] = 0
+    current_index = 0
+    arr_len = len(arr)
     print(arr)
+
+    while current_index < arr_len:
+        if arr[current_index] == 0:
+            remove_num = arr.pop(current_index)
+            arr.append(remove_num)
+            arr_len -= 1
+            print(arr)
+        else:
+            current_index += 1
+    print(arr)
+    return arr
 
 
 if __name__ == '__main__':
