5cb8d86914b7878ba80c6f9ed8c4f80738f58147
Tyler Alsop <tylerjalsop@gmail.com>
2020-07-17 10:46:31 -0600

    Filled out the understand portion of sliding_window_max.py.

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,6 +4,18 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    # U:
+        # We recieve an array of numbers, and a separate number (k).
+        # Moving from left to right across the array:
+            # We look at the first k numbers (i.e. If k equals 3 then we compare the first 3 numbers. If k is 5 then we compare the first 5 numbers).
+            # Then we compare at the next k-number of numbers starting with arr[1]
+            # Then we compare at the next k-number of numbers starting with arr[2]
+            # Then we compare at the next k-number of numbers starting with arr[3], etc etc. until the k-th number reaches the end of the array (arr[len(arr)-1]).
+            # The function needs to return an array of numbers consisting of the largest number of each comparison.
+        # The length of the new array outputed will be the length of the original array minus the length of k minus 1: 
+            # len(output) = len(arr) - (k-1)
+    # P:
+        
 
     pass
 
